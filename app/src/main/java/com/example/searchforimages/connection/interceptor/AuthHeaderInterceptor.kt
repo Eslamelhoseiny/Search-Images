bab6b0406ad01a71b932ce272e611d70bcbbacaf
Your Real Name Here <Your Email Address Here>
2019-12-03 12:27:17 +0200

    Add Client Auth Header

--- app/src/main/java/com/example/searchforimages/connection/interceptor/AuthHeaderInterceptor.kt
+++ app/src/main/java/com/example/searchforimages/connection/interceptor/AuthHeaderInterceptor.kt
@@ -5,9 +5,10 @@ import okhttp3.Response
 
 class AuthHeaderInterceptor : Interceptor {
 
+    //2758066fa9b683235a4802390cc02bfe9c54287e
     override fun intercept(chain: Interceptor.Chain): Response {
         val ongoing = chain.request().newBuilder()
-        ongoing.header("Authorization", "Client-ID 37398025a821c1b")
+        ongoing.header("Authorization", "Client-ID 8030ef25d77188c")
         return chain.proceed(ongoing.build())
     }
 
